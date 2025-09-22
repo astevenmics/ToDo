50c5494333fa4eefdc06ee1622b9357444b02606
astevenmics <aljohnstevenmiclat.kes@gmail.com>
2025-09-21 22:58:39 -0500

    === ToDo 0.0.0.3.33 === - ItemFunctions * Added getItem function to reduce code duplication in MainFunction.java by handling inputs from users * Renamed updateItemDescription to updateItemTitle

--- src/ItemFunctions.java
+++ src/ItemFunctions.java
@@ -1,11 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ItemFunctions {
 
     public void setCompleted(Item item) {
         item.setComplete(true);
     }
 
-    public void updateItemDescription(Item item, String description) {
-        item.setTitle(description);
+    public void updateItemTitle(Item item, String title) {
+        item.setTitle(title);
+    }
+
+    public Item getItem(Scanner scanner, ArrayList<Item> todo, String extensionMessage) {
+        while (true) {
+            try {
+                System.out.print("Type the number of the item you want to " + extensionMessage + ": ");
+                int itemNumber = Integer.parseInt(scanner.nextLine());
+
+                return todo.get(itemNumber - 1);
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println("Please enter a whole number!");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Item does not exist in the list!");
+            }
+        }
     }
 
 }
